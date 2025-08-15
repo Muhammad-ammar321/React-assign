23dd0e508957125f9174a210fa61f1647863d7e9
Muhammad-ammar321 <mammarshamim@gmail.com>
2025-08-15 22:00:12 +0500

    React assignment 1a

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import Navbar from './components/assignment a1/Navbar'
+import Hero from './components/assignment a1/Hero'
+import Feature from './components/assignment a1/Feature'
+import FAQ from './components/assignment a1/FAQ'
+import NewsLetter from './components/assignment a1/Newsletter'
+import Footer from './components/assignment a1/footer'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Feature />
+      <FAQ />
+      <NewsLetter />
+      <Footer />
+    </>
+  )
+}
+
+export default App
